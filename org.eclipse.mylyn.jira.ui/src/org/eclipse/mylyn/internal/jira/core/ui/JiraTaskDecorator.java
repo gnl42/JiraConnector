a19a48187a172df0d9e9aa82bd9f592055fea346
Mik Kersten <mik.kersten@tasktop.com>
2006-12-07 20:24:37 +0000

    NEW - bug 142661: Reduce the dependancy of Bugzilla search on Eclipse https://bugs.eclipse.org/bugs/show_bug.cgi?id=142661

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/core/ui/JiraTaskDecorator.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/core/ui/JiraTaskDecorator.java
@@ -31,13 +31,13 @@ public class JiraTaskDecorator implements ILightweightLabelDecorator {
 		}
 		if (element instanceof JiraTask) {
 			JiraTask task = (JiraTask) element;
-			if (JiraTask.Kind.BUG.toString().equals(task.getTaskType())) {
+			if (JiraTask.Kind.BUG.toString().equals(task.getTaskKind())) {
 				decoration.addOverlay(JiraImages.OVERLAY_BUG, IDecoration.BOTTOM_RIGHT);
-			} else if (JiraTask.Kind.FEATURE.toString().equals(task.getTaskType())) {
+			} else if (JiraTask.Kind.FEATURE.toString().equals(task.getTaskKind())) {
 				decoration.addOverlay(JiraImages.OVERLAY_FEATURE, IDecoration.BOTTOM_RIGHT);
-			} else if (JiraTask.Kind.IMPROVEMENT.toString().equals(task.getTaskType())) {
+			} else if (JiraTask.Kind.IMPROVEMENT.toString().equals(task.getTaskKind())) {
 				decoration.addOverlay(JiraImages.OVERLAY_IMPROVEMENT, IDecoration.BOTTOM_RIGHT);
-			} else if (JiraTask.Kind.TASK.toString().equals(task.getTaskType())) {
+			} else if (JiraTask.Kind.TASK.toString().equals(task.getTaskKind())) {
 				decoration.addOverlay(JiraImages.OVERLAY_TASK, IDecoration.BOTTOM_RIGHT);
 			}
 		}
