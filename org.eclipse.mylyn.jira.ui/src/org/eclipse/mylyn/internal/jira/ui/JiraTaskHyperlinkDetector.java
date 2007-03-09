604e2f8c8b4598ddfd17ce9861c56c766ee7a911
Robert Elves <robert.elves@tasktop.com>
2007-03-09 22:41:33 +0000

    NEW - bug 165380: improve linking between Task Editor and Task List https://bugs.eclipse.org/bugs/show_bug.cgi?id=165380

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/JiraTaskHyperlinkDetector.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/JiraTaskHyperlinkDetector.java
@@ -8,105 +8,55 @@
 
 package org.eclipse.mylar.internal.jira.ui;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.Region;
-import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.mylar.tasks.core.AbstractRepositoryConnector;
 import org.eclipse.mylar.tasks.core.TaskRepository;
 import org.eclipse.mylar.tasks.ui.TasksUiPlugin;
-import org.eclipse.mylar.tasks.ui.editors.RepositoryTextViewer;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.mylar.tasks.ui.editors.AbstractTaskHyperlinkDetector;
 
 /**
  * @author Eugene Kuleshov
  */
-public class JiraTaskHyperlinkDetector extends AbstractHyperlinkDetector {
+public class JiraTaskHyperlinkDetector extends AbstractTaskHyperlinkDetector {
 
-	public IHyperlink[] detectHyperlinks(ITextViewer textViewer, IRegion region, boolean canShowMultipleHyperlinks) {
-		if (region == null || textViewer == null) {
-			return null;
-		}
-
-		TaskRepository repository = null;
-		if (textViewer instanceof RepositoryTextViewer) {
-			// Get repository from repository text viewer
-			RepositoryTextViewer viewer = (RepositoryTextViewer) textViewer;
-
-			repository = viewer.getRepository();
-
-			if (repository == null)
-				return null;
-
-		} else {
-			// Get repository from files associated project -> repository
-			// mapping
-			IEditorPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-			IEditorInput input = part.getEditorInput();
-			IResource resource = (IResource) input.getAdapter(IResource.class);
-			if (resource != null) {
-				repository = TasksUiPlugin.getDefault().getRepositoryForResource(resource, true);
-				if (repository == null) {
-					return null;
-				}
-			}
-		}
-
-		if (repository == null)
-			return null;
-
-		IDocument document = textViewer.getDocument();
-		if (document == null)
-			return null;
-
-		int offset = region.getOffset();
-		IRegion lineInfo;
-		String line;
-		try {
-			lineInfo = document.getLineInformationOfOffset(offset);
-			line = document.get(lineInfo.getOffset(), lineInfo.getLength());
-		} catch (BadLocationException ex) {
-			return null;
-		}
-
-		int offsetInLine = offset - lineInfo.getOffset();
-
+	@Override
+	protected IHyperlink[] findHyperlinks(TaskRepository repository, String text, int lineOffset, int regionOffset) {
 		AbstractRepositoryConnector connector = TasksUiPlugin.getRepositoryManager().getRepositoryConnector(
 				repository.getKind());
 
-		int startPos = offsetInLine;
+		int startPos = lineOffset;
 		while (startPos > 0) {
-			char c = line.charAt(startPos);
+			char c = text.charAt(startPos);
 			if (Character.isWhitespace(c) || ",.;[](){}".indexOf(c) > -1)
 				break;
 			startPos--;
 		}
-		int endPos = offsetInLine;
-		while (endPos < lineInfo.getLength()) {
-			char c = line.charAt(endPos);
+		int endPos = lineOffset;
+		while (endPos < text.length()) {
+			char c = text.charAt(endPos);
 			if (Character.isWhitespace(c) || ",.;[](){}".indexOf(c) > -1)
 				break;
 			endPos++;
 		}
 
-		String[] taskIds = connector.getTaskIdsFromComment(repository, line.substring(startPos, endPos));
+		String[] taskIds = connector.getTaskIdsFromComment(repository, text.substring(startPos, endPos));
 		if (taskIds == null || taskIds.length == 0)
 			return null;
 
 		IHyperlink[] links = new IHyperlink[taskIds.length];
 		for (int i = 0; i < taskIds.length; i++) {
 			String taskId = taskIds[i];
-			int startRegion = line.indexOf(taskId, startPos);
-			links[i] = new JiraHyperLink(new Region(lineInfo.getOffset() + startRegion, taskId.length()), repository,
-					taskId, connector.getTaskWebUrl(repository.getUrl(), taskId));
+			int startRegion = text.indexOf(taskId, startPos);
+			links[i] = new JiraHyperLink(new Region(regionOffset + startRegion, taskId.length()), repository, taskId,
+					connector.getTaskWebUrl(repository.getUrl(), taskId));
 		}
 		return links;
+	}
+
+	@Override
+	protected String getTargetID() {
+		return JiraUiPlugin.REPOSITORY_KIND;
 	}
 
 }
