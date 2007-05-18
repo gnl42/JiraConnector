9312a76f62b0eeff84f6a215a1fa6bdc7aba621f
Steffen Pingel <steffen.pingel@tasktop.com>
2007-05-18 04:00:33 +0000

    NEW - bug 186931: Could not download report error in the log https://bugs.eclipse.org/bugs/show_bug.cgi?id=186931

---
+++ org.eclipse.mylyn.jira.tests/src/org/eclipse/mylyn/jira/tests/HTML2TextReaderTest.java
@@ -0,0 +1,45 @@
+package org.eclipse.mylar.jira.tests;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+import org.eclipse.mylar.internal.jira.ui.html.HTML2TextReader;
+
+public class HTML2TextReaderTest extends TestCase {
+
+	public void testReadOnlyWhitespacesWithSkip() throws Exception {
+		String text = " ";
+		StringReader stringReader = new StringReader(text);
+		HTML2TextReader html2TextReader = new HTML2TextReader(stringReader, null) {
+			@Override
+			public int read(char[] cbuf, int off, int len) throws IOException {
+				setSkipWhitespace(true);
+				return super.read(cbuf, off, len);
+			}
+		};
+		char[] chars = new char[text.length()];
+		int len = html2TextReader.read(chars, 0, text.length());
+		assertEquals(-1, len);
+	}
+
+	public void testReadOnlyWhitespaces() throws Exception {
+		String text = " ";
+		StringReader stringReader = new StringReader(text);
+		HTML2TextReader html2TextReader = new HTML2TextReader(stringReader, null);
+		char[] chars = new char[text.length()];
+		int len = html2TextReader.read(chars, 0, text.length());
+		assertEquals(1, len);
+		assertEquals(" ", new String(chars, 0, len));
+		
+		text = "&nbsp;";
+		stringReader = new StringReader(text);
+		html2TextReader = new HTML2TextReader(stringReader, null);
+		chars = new char[text.length()];
+		len = html2TextReader.read(chars, 0, text.length());
+		assertEquals(1, len);
+		assertEquals(" ", new String(chars, 0, len));
+	}
+
+}
