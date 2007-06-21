1c3d950e8092a99358a4dae6e6325d26e9b76809
Mik Kersten <mik.kersten@tasktop.com>
2007-06-21 07:21:46 +0000

    NEW - bug 166609: [api] improve task externalization API https://bugs.eclipse.org/bugs/show_bug.cgi?id=166609

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/JiraTaskListFactory.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/JiraTaskListFactory.java
@@ -13,39 +13,32 @@ package org.eclipse.mylyn.internal.jira.ui;
 
 import java.io.ByteArrayInputStream;
 import java.io.ObjectInputStream;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.mylyn.internal.jira.core.model.NamedFilter;
 import org.eclipse.mylyn.internal.jira.core.model.filter.FilterDefinition;
-import org.eclipse.mylyn.internal.monitor.core.util.StatusManager;
 import org.eclipse.mylyn.tasks.core.AbstractRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.AbstractTask;
-import org.eclipse.mylyn.tasks.core.AbstractTaskContainer;
-import org.eclipse.mylyn.tasks.core.DelegatingTaskExternalizer;
+import org.eclipse.mylyn.tasks.core.AbstractTaskListFactory;
 import org.eclipse.mylyn.tasks.core.TaskExternalizationException;
-import org.eclipse.mylyn.tasks.core.TaskList;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.ui.TasksUiPlugin;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 /**
  * @author Mik Kersten
  * @author Wesley Coelho (filter prototyping)
  */
-public class JiraTaskExternalizer extends DelegatingTaskExternalizer {
+public class JiraTaskListFactory extends AbstractTaskListFactory {
 
 	private static final String KEY_JIRA = "Jira";
 
 	private static final String KEY_CUSTOM = "JiraCustom";
 
-	private static final String KEY_JIRA_CATEGORY = "JiraQuery" + KEY_CATEGORY;
+	private static final String KEY_JIRA_QUERY = KEY_JIRA + AbstractTaskListFactory.KEY_QUERY;
 
-//	private static final String KEY_JIRA_QUERY_HIT = KEY_JIRA + KEY_QUERY_HIT;
-
-	private static final String KEY_JIRA_QUERY = KEY_JIRA + KEY_QUERY;
-
-	private static final String KEY_JIRA_CUSTOM = KEY_JIRA + KEY_CUSTOM + KEY_QUERY;
+	private static final String KEY_JIRA_CUSTOM = KEY_JIRA + KEY_CUSTOM + AbstractTaskListFactory.KEY_QUERY;
 
 	private static final String KEY_JIRA_ISSUE = "JiraIssue";
 
@@ -60,37 +53,30 @@ public class JiraTaskExternalizer extends DelegatingTaskExternalizer {
 	private static final String KEY_KEY = "Key";
 
 	@Override
-	public boolean canReadQuery(Node node) {
-		return node.getNodeName().equals(KEY_JIRA_QUERY) || node.getNodeName().equals(KEY_JIRA_CUSTOM);
+	public String getTaskElementName() {
+		return KEY_JIRA_ISSUE;
+	}
+	
+	@Override
+	public Set<String> getQueryElementNames() {
+		Set<String> names = new HashSet<String>();
+		names.add(KEY_JIRA_QUERY);
+		names.add(KEY_JIRA_CUSTOM);
+		return names;
 	}
 
-// @Override
-// public boolean canCreateElementFor(AbstractTask queryHit) {
-// return queryHit instanceof JiraQueryHit;
-// }
-
-// @Override
-// public Element createQueryHitElement(AbstractTask queryHit,
-// Document doc, Element parent) {
-// Element node = super.createQueryHitElement(queryHit, doc, parent);
-// node.setAttribute(KEY_KEY, ((JiraTask) queryHit).getKey());
-// return node;
-// }
-
 	@Override
-	public boolean canCreateElementFor(AbstractRepositoryQuery category) {
+	public boolean canCreate(AbstractRepositoryQuery category) {
 		return category instanceof JiraRepositoryQuery || category instanceof JiraCustomQuery;
 	}
 
 	@Override
-	public boolean canCreateElementFor(AbstractTask task) {
+	public boolean canCreate(AbstractTask task) {
 		return task instanceof JiraTask;
 	}
 
 	@Override
-	public AbstractRepositoryQuery readQuery(Node node, TaskList taskList) throws TaskExternalizationException {
-		Element element = (Element) node;
-		String repositoryUrl = element.getAttribute(KEY_REPOSITORY_URL);
+	public AbstractRepositoryQuery createQuery(String repositoryUrl, String queryString, String label, Element element) throws TaskExternalizationException {
 		String custom = element.getAttribute(KEY_FILTER_CUSTOM);
 		String customUrl = element.getAttribute(KEY_FILTER_CUSTOM_URL);
 		AbstractRepositoryQuery query;
@@ -123,41 +109,19 @@ public class JiraTaskExternalizer extends DelegatingTaskExternalizer {
 	}
 
 	@Override
-	public Element createQueryElement(AbstractRepositoryQuery query, Document doc, Element parent) {
-		String queryTagName = getQueryTagNameForElement(query);
-		Element node = doc.createElement(queryTagName);
-
-		node.setAttribute(KEY_NAME, query.getSummary());
-// node.setAttribute(KEY_QUERY_MAX_HITS, query.getMaxHits() + "");
-		node.setAttribute(KEY_QUERY_STRING, query.getUrl());
-		node.setAttribute(KEY_REPOSITORY_URL, query.getRepositoryUrl());
+	public void setAdditionalAttributes(AbstractRepositoryQuery query, Element node) {
+		String queryTagName = getQueryElementName(query);
 
 		if (query instanceof JiraRepositoryQuery) {
 			NamedFilter filter = ((JiraRepositoryQuery) query).getNamedFilter();
 			node.setAttribute(KEY_FILTER_ID, filter.getId());
 			node.setAttribute(KEY_FILTER_NAME, filter.getName());
-			// node.setAttribute(KEY_FILTER_DESCRIPTION,
-			// filter.getDescription());
 		} else if (query instanceof JiraCustomQuery) {
 			JiraCustomQuery customQuery = (JiraCustomQuery) query;
-			// FilterDefinition filter = customQuery.getFilterDefinition();
 			node.setAttribute(KEY_FILTER_ID, customQuery.getSummary());
 			node.setAttribute(KEY_FILTER_NAME, customQuery.getSummary());
-			// node.setAttribute(KEY_FILTER_DESCRIPTION,
-			// filter.getDescription());
 			node.setAttribute(KEY_FILTER_CUSTOM_URL, customQuery.getUrl());
 		}
-
-		for (AbstractTask hit : query.getChildren()) {
-			try {
-				createQueryHitElement(hit, doc, node);
-			} catch (Exception e) {
-				StatusManager.log(e, e.getMessage());
-			}
-		}
-
-		parent.appendChild(node);
-		return node;
 	}
 
 	private FilterDefinition decodeFilter(String filter) {
@@ -178,49 +142,23 @@ public class JiraTaskExternalizer extends DelegatingTaskExternalizer {
 	}
 
 	@Override
-	public String getTaskTagName() {
-		return KEY_JIRA_ISSUE;
+	public void setAdditionalAttributes(AbstractTask task, Element element) {
+		element.setAttribute(KEY_KEY, ((JiraTask) task).getTaskKey());
 	}
 
 	@Override
-	public Element createTaskElement(AbstractTask task, Document doc, Element parent) {
-		Element node = super.createTaskElement(task, doc, parent);
-		node.setAttribute(KEY_KEY, ((JiraTask) task).getTaskKey());
-		return node;
-	}
-
-	@Override
-	public AbstractTask createTask(String repositoryUrl, String taskId, String summary, Element element, TaskList taskList,
-			AbstractTaskContainer category, AbstractTask parent) throws TaskExternalizationException {
+	public AbstractTask createTask(String repositoryUrl, String taskId, String summary, Element element) throws TaskExternalizationException {
 		JiraTask task = new JiraTask(repositoryUrl, taskId, summary);
 
 		if (element.hasAttribute(KEY_KEY)) {
 			String key = element.getAttribute(KEY_KEY);
 			task.setTaskKey(key);
-		} else {
-			// ignore if key not found
-		}
+		} 
 		return task;
 	}
 
-//	@Override
-//	public boolean canReadQueryHit(Node node) {
-//		return node.getNodeName().equals(getQueryHitTagName());
-//	}
-
-//	@Override
-//	public AbstractTask createQueryHit(String repositoryUrl, String taskId, String summary, Element element,
-//			TaskList taskList, AbstractRepositoryQuery query) throws TaskExternalizationException {
-//		String key = "";
-//		if (element.hasAttribute(KEY_KEY)) {
-//			key = element.getAttribute(KEY_KEY);
-//		}
-//
-//		return new JiraQueryHit(taskList, summary, repositoryUrl, taskId, key);
-//	}
-
 	@Override
-	public String getQueryTagNameForElement(AbstractRepositoryQuery query) {
+	public String getQueryElementName(AbstractRepositoryQuery query) {
 		if (query instanceof JiraRepositoryQuery) {
 			return KEY_JIRA_QUERY;
 		} else if (query instanceof JiraCustomQuery) {
@@ -228,14 +166,4 @@ public class JiraTaskExternalizer extends DelegatingTaskExternalizer {
 		}
 		return "";
 	}
-
-	@Override
-	public String getCategoryTagName() {
-		return KEY_JIRA_CATEGORY;
-	}
-
-//	@Override
-//	public String getQueryHitTagName() {
-//		return KEY_JIRA_QUERY_HIT;
-//	}
 }
