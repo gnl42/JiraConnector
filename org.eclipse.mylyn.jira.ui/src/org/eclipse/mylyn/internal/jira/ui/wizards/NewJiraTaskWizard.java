4870a7c2455065176ba9dc0ad6e7409e47002762
Mik Kersten <mik.kersten@tasktop.com>
2007-06-26 01:16:44 +0000

    RESOLVED - bug 194157: update copyright notices https://bugs.eclipse.org/bugs/show_bug.cgi?id=194157

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/NewJiraTaskWizard.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/NewJiraTaskWizard.java
@@ -1,12 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2004 - 2006 University Of British Columbia and others.
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     University Of British Columbia - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.mylyn.internal.jira.ui.wizards;
