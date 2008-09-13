ada3f05b0bc3bf746ee7a2079620bb8c5782cea8
Mik Kersten <mik.kersten@tasktop.com>
2008-09-13 03:28:28 +0000

    NEW - bug 194157: update copyright notices to 2008 https://bugs.eclipse.org/bugs/show_bug.cgi?id=194157

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/JiraFilterDefinitionPage.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/JiraFilterDefinitionPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2004, 2008 Tasktop Technologies and others.
+ * Copyright (c) 2004, 2008 Brock Janiczak and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,7 @@
  * Contributors:
  *     Brock Janiczak - initial API and implementation
  *     Eugene Kuleshov - improvements
- *     Tasktop Technologis - improvements
+ *     Tasktop Technologies - improvements
  *******************************************************************************/
 
 package org.eclipse.mylyn.internal.jira.ui.wizards;
