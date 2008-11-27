2826d8505c3ae52c6ae98a129e684f765b702f64
Shawn Minto <shawn.minto@tasktop.com>
2008-11-27 22:37:11 +0000

    RESOLVED - issue PLE-63: setup PLE projects as eclipse plugins https://studio.atlassian.com/browse/PLE-63

---
+++ com.atlassian.connector.eclipse.crucible.core/src/com/atlassian/connector/eclipse/internal/crucible/core/CrucibleCorePlugin.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Atlassian and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlassian - initial API and implementation
+ ******************************************************************************/
+
+package com.atlassian.connector.eclipse.internal.crucible.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ * 
+ * @author Shawn Minto
+ */
+public class CrucibleCorePlugin extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "asds";
+
+	// The shared instance
+	private static CrucibleCorePlugin plugin;
+
+	/**
+	 * The constructor
+	 */
+	public CrucibleCorePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static CrucibleCorePlugin getDefault() {
+		return plugin;
+	}
+
+}
