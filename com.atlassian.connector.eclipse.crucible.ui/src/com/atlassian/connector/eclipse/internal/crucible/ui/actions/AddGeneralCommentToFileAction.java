70d76d5ea5b86538b89548d214219804dde29e4e
Shawn Minto <shawn.minto@tasktop.com>
2009-01-30 00:12:24 +0000

    fix add general comment from within a file

--- com.atlassian.connector.eclipse.crucible.ui/src/com/atlassian/connector/eclipse/internal/crucible/ui/actions/AddGeneralCommentToFileAction.java
+++ com.atlassian.connector.eclipse.crucible.ui/src/com/atlassian/connector/eclipse/internal/crucible/ui/actions/AddGeneralCommentToFileAction.java
@@ -11,13 +11,20 @@
 
 package com.atlassian.connector.eclipse.internal.crucible.ui.actions;
 
-import com.atlassian.connector.eclipse.internal.crucible.ui.IReviewActionListener;
+import com.atlassian.connector.eclipse.internal.crucible.ui.CrucibleUiPlugin;
 import com.atlassian.connector.eclipse.internal.crucible.ui.IReviewAction;
+import com.atlassian.connector.eclipse.internal.crucible.ui.IReviewActionListener;
 import com.atlassian.connector.eclipse.ui.team.CrucibleFile;
+import com.atlassian.connector.eclipse.ui.team.TeamUiUtils;
 import com.atlassian.theplugin.commons.crucible.api.model.Review;
 
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.text.source.LineRange;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.mylyn.tasks.ui.TasksUiImages;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
 
 /**
  * Action to add a general file comment to the active review
@@ -27,14 +34,63 @@ import org.eclipse.mylyn.tasks.ui.TasksUiImages;
  */
 public class AddGeneralCommentToFileAction extends AbstractAddCommentAction implements IReviewAction {
 
-	private final CrucibleFile crucibleFile;
+	private CrucibleFile crucibleFile;
 
 	private IReviewActionListener actionListener;
+
+	public AddGeneralCommentToFileAction() {
+		super("Create General File Comment");
+		crucibleFile = null;
+	}
 
 	public AddGeneralCommentToFileAction(CrucibleFile file, Review review) {
 		super("Create General File Comment");
 		super.review = review;
 		this.crucibleFile = file;
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		super.selectionChanged(action, selection);
+		if (action.isEnabled() && isEnabled()) {
+			IEditorPart editorPart = getActiveEditor();
+			IEditorInput editorInput = getEditorInputFromSelection(selection);
+			if (editorInput != null && editorPart != null) {
+				crucibleFile = TeamUiUtils.getCorrespondingCrucibleFileFromEditorInput(editorInput,
+						CrucibleUiPlugin.getDefault().getActiveReviewManager().getActiveReview());
+				if (crucibleFile != null) {
+					action.setEnabled(true);
+					setEnabled(true);
+					return;
+				}
+			}
+			action.setEnabled(false);
+			setEnabled(false);
+			crucibleFile = null;
+		} else {
+			action.setEnabled(false);
+			setEnabled(false);
+			crucibleFile = null;
+		}
+	}
+
+	@Override
+	protected Review getReview() {
+		if (review != null) {
+			return review;
+		} else {
+			return CrucibleUiPlugin.getDefault().getActiveReviewManager().getActiveReview();
+		}
+	}
+
+	@Override
+	protected CrucibleFile getCrucibleFile() {
+		return crucibleFile;
+	}
+
+	@Override
+	protected LineRange getSelectedRange() {
+		return null;
 	}
 
 	@Override
@@ -58,16 +114,6 @@ public class AddGeneralCommentToFileAction extends AbstractAddCommentAction impl
 	@Override
 	public String getToolTipText() {
 		return "Add General File Comment";
-	}
-
-	@Override
-	protected Review getReview() {
-		return review;
-	}
-
-	@Override
-	protected CrucibleFile getCrucibleFile() {
-		return crucibleFile;
 	}
 
 	public void setActionListener(IReviewActionListener listener) {
