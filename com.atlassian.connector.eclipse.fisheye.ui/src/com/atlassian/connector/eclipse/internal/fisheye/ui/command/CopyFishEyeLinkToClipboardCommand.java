e67014eafb3e601e4a8c9748065f87be89c3a1fc
Wojciech Seliga <wseliga@atlassian.com>
2009-04-24 12:29:22 +0000

    PLE-397, PLE-398 under dev

---
+++ com.atlassian.connector.eclipse.fisheye.ui/src/com/atlassian/connector/eclipse/internal/fisheye/ui/command/CopyFishEyeLinkToClipboardCommand.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Atlassian and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlassian - initial API and implementation
+ ******************************************************************************/
+
+package com.atlassian.connector.eclipse.internal.fisheye.ui.command;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CopyFishEyeLinkToClipboardCommand extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+}
