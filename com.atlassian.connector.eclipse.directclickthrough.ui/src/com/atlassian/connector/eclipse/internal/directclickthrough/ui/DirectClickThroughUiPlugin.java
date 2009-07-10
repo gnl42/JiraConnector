0c7ef565506e0333d20a97d099211d39790448f2
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-07-10 13:39:32 +0000

    OPEN - issue PLE-591: Copy HTTP embedded server from ACI to ACE https://studio.atlassian.com/browse/PLE-591

--- com.atlassian.connector.eclipse.directclickthrough.ui/src/com/atlassian/connector/eclipse/internal/directclickthrough/ui/DirectClickThroughUiPlugin.java
+++ com.atlassian.connector.eclipse.directclickthrough.ui/src/com/atlassian/connector/eclipse/internal/directclickthrough/ui/DirectClickThroughUiPlugin.java
@@ -1,17 +1,7 @@
 package com.atlassian.connector.eclipse.internal.directclickthrough.ui;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Map;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.equinox.http.jetty.JettyConfigurator;
-import org.eclipse.mylyn.commons.core.StatusHandler;
+import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -26,6 +16,13 @@ public class DirectClickThroughUiPlugin extends AbstractUIPlugin {
 	// The shared instance
 	private static DirectClickThroughUiPlugin plugin;
 	
+	public static class EarlyStartup implements IStartup {
+
+		public void earlyStartup() {
+			// force early loading
+		}
+	}
+	
 	/**
 	 * The constructor
 	 */
@@ -39,6 +36,25 @@ public class DirectClickThroughUiPlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		/*
+		if (getPreferenceStore().getBoolean(IDirectClickThroughPreferenceConstants.ENABLED)) {
+			Job serverJob = new Job("Start Embedded Web Server") {
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					try {
+						Hashtable<String, Object> params = new Hashtable<String, Object>();
+						params.put("http.port", getPreferenceStore().getInt(IDirectClickThroughPreferenceConstants.PORT_NUMBER));
+						params.put("http.host", InetAddress.getByName(null).toString());
+						JettyConfigurator.startServer(PLUGIN_ID, params); 
+					} catch (Exception e) {
+						StatusHandler.log(new Status(IStatus.ERROR, DirectClickThroughUiPlugin.PLUGIN_ID, 
+								"Unable to run embedded web server, Direct Click Through will not be available", e));
+					}
+					return null;
+				}
+			};
+			serverJob.schedule();
+		}*/
 	}
 
 	/*
