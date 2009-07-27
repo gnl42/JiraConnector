95034e34270545c30253f627dd7ab2de6c4f6627
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-07-27 10:21:43 +0000

    PLE-598: project settings

--- com.atlassian.connector.eclipse.monitor.server/src/main/java/com/atlassian/connector/eclipse/monitor/server/servlet/UploadServlet.java
+++ com.atlassian.connector.eclipse.monitor.server/src/main/java/com/atlassian/connector/eclipse/monitor/server/servlet/UploadServlet.java
@@ -1,10 +1,19 @@
 package com.atlassian.connector.eclipse.monitor.server.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class UploadServlet
@@ -30,7 +39,24 @@ public class UploadServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		if (ServletFileUpload.isMultipartContent(request)) {
+			return;
+		}
+		
+		FileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		
+		try {
+			List<?> files = upload.parseRequest(request);
+			if (files != null) {
+				for (Object fileObj : files) {
+					FileItem file = (FileItem) fileObj;
+					// FIXME: do nothing
+				}
+			}
+		} catch(FileUploadException e) {
+			
+		}
 	}
 
 }
