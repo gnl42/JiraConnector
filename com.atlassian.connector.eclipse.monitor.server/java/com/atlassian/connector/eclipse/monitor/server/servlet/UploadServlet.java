2ab21848eaae491c4a0b02c6b64f0090233d4558
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-07-27 08:18:38 +0000

    PLE-598: importing server draft

---
+++ com.atlassian.connector.eclipse.monitor.server/java/com/atlassian/connector/eclipse/monitor/server/servlet/UploadServlet.java
@@ -0,0 +1,36 @@
+package com.atlassian.connector.eclipse.monitor.server.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public UploadServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
