6249e7b62a61eb8ee641eeeddf3673f2ea7c5c92
Steffen Pingel <steffen.pingel@tasktop.com>
2009-08-20 21:39:45 +0000

    RESOLVED - bug 219385: [e3.4] remove reflective legacy calls https://bugs.eclipse.org/bugs/show_bug.cgi?id=219385

--- org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/JiraProjectPage.java
+++ org.eclipse.mylyn.jira.ui/src/org/eclipse/mylyn/internal/jira/ui/wizards/JiraProjectPage.java
@@ -45,6 +45,7 @@ import org.eclipse.mylyn.internal.jira.core.service.JiraException;
 import org.eclipse.mylyn.internal.jira.core.util.JiraUtil;
 import org.eclipse.mylyn.internal.jira.ui.JiraUiPlugin;
 import org.eclipse.mylyn.internal.provisional.commons.ui.CommonUiUtil;
+import org.eclipse.mylyn.internal.provisional.commons.ui.EnhancedFilteredTree;
 import org.eclipse.mylyn.internal.provisional.commons.ui.ICoreRunnable;
 import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.ITask;
@@ -112,7 +113,7 @@ public class JiraProjectPage extends WizardPage {
 			}
 
 		};
-		projectTree = new FilteredTree(composite, SWT.SINGLE | SWT.BORDER, patternFilter, true);
+		projectTree = new EnhancedFilteredTree(composite, SWT.SINGLE | SWT.BORDER, patternFilter, true);
 		projectTree.setLayoutData(GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).hint(
 				SWT.DEFAULT, 200).create());
 
