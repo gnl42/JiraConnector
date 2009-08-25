28e202de333748d3ccbdaadce09afa2b5021599c
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-08-25 14:56:44 +0000

    NONE: fixed buttons order in DCT initial dialog

--- com.atlassian.connector.eclipse.monitor.usage/src/com/atlassian/connector/eclipse/internal/monitor/usage/dialogs/AskForPermissionDialog.java
+++ com.atlassian.connector.eclipse.monitor.usage/src/com/atlassian/connector/eclipse/internal/monitor/usage/dialogs/AskForPermissionDialog.java
@@ -83,8 +83,8 @@ public class AskForPermissionDialog extends Dialog {
 
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.NO_ID, IDialogConstants.NO_LABEL, false);
 		createButton(parent, IDialogConstants.YES_ID, IDialogConstants.YES_LABEL, true);
+		createButton(parent, IDialogConstants.NO_ID, IDialogConstants.NO_LABEL, false);
 	}
 
 	@Override
