84953dd8570841c403ce42feca4d8e57daad6b5c
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-11-02 12:42:21 +0000

    PLE-735

--- com.atlassian.connector.eclipse.subversive.core/src/com/atlassian/connector/eclipse/internal/subversive/core/SubversiveUtil.java
+++ com.atlassian.connector.eclipse.subversive.core/src/com/atlassian/connector/eclipse/internal/subversive/core/SubversiveUtil.java
@@ -1,14 +1,20 @@
 package com.atlassian.connector.eclipse.internal.subversive.core;
 
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.mylyn.commons.core.StatusHandler;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.team.core.RepositoryProvider;
 import org.eclipse.team.svn.core.SVNTeamPlugin;
 import org.eclipse.team.svn.core.connector.SVNRevision;
@@ -17,6 +23,8 @@ import org.eclipse.team.svn.core.resource.IRepositoryLocation;
 import org.eclipse.team.svn.core.resource.IRepositoryResource;
 import org.eclipse.team.svn.core.svnstorage.SVNRemoteStorage;
 import org.eclipse.team.svn.core.svnstorage.SVNRepositoryFile;
+
+import com.atlassian.theplugin.commons.util.MiscUtil;
 
 public final class SubversiveUtil {
 	
@@ -36,39 +44,85 @@ public final class SubversiveUtil {
 		return null;
 	}
 	
-	public static IResource getLocalResourceFromFilePath(String filePath) {
-		if (filePath == null || filePath.length() <= 0) {
+	public static IResource getLocalResourceFromFilePath(final String path) {
+		if (path == null || path.length() <= 0) {
+			StatusHandler.log(new Status(IStatus.ERROR, AtlassianSubversiveCorePlugin.PLUGIN_ID, "Requested file path is null or empty."));
 			return null;
 		}
-		for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
-			// check if project is associated with Subversive Team provider, 
-			// if we don't test it asRepositoryResource will throw RuntimeException
-			RepositoryProvider provider = RepositoryProvider.getProvider(project, SVNTeamPlugin.NATURE_ID);
-			if (provider == null) {
-				continue;
-			}
-
-			try {
-				IPath fileIPath = new Path(filePath);
-				IResource resource = project.findMember(fileIPath);
-				while (!fileIPath.isEmpty() && resource == null) {
-					fileIPath = fileIPath.removeFirstSegments(1);
-					resource = project.findMember(fileIPath);
-				}
-				if (resource == null) {
-					continue;
-				}
-
-				IRepositoryResource projectResource = SVNRemoteStorage.instance().asRepositoryResource(resource);
-
-				if (projectResource != null && projectResource.getUrl() != null
-						&& projectResource.getUrl().endsWith(filePath)) {
-					return resource;
-				}
-			} catch (Exception e) {
-				StatusHandler.log(new Status(IStatus.ERROR, AtlassianSubversiveCorePlugin.PLUGIN_ID, e.getMessage(), e));
-			}
+		
+		final IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		if (projects == null || projects.length == 0) {
+			StatusHandler.log(new Status(IStatus.WARNING, AtlassianSubversiveCorePlugin.PLUGIN_ID, "Could not find projects in the workspace."));
 		}
+		
+		final List<IResource> resources = MiscUtil.buildArrayList();
+		final int[] matchLevel = new int[] { 0 };
+
+		try {
+			ResourcesPlugin.getWorkspace().getRoot().accept(
+				new IResourceVisitor() {
+					private int matchingLastSegments(
+							IPath firstPath, IPath secondPath) {
+						int firstPathLen = firstPath.segmentCount();
+						int secondPathLen = secondPath
+								.segmentCount();
+						int max = Math.min(firstPathLen,
+								secondPathLen);
+						int count = 0;
+						for (int i = 1; i <= max; i++) {
+							if (!firstPath
+									.segment(firstPathLen - i)
+									.equals(
+											secondPath
+													.segment(secondPathLen
+															- i))) {
+								return count;
+							}
+							count++;
+						}
+						return count;
+					}
+
+					public boolean visit(IResource resource)
+							throws CoreException {
+						if (resource.getType() == IResource.PROJECT 
+								&& RepositoryProvider.getProvider(resource.getProject(), SVNTeamPlugin.NATURE_ID) == null) {
+							return false;
+						}
+						
+						if (!(resource instanceof IFile)) {
+							return true; // skip it if it's not a
+											// file, but check its
+											// members
+						}
+
+						int matchCount = matchingLastSegments(
+								new Path(path), resource
+										.getLocation());
+						if (matchCount > 0) {
+							if (matchCount > matchLevel[0]) {
+								resources.clear();
+								matchLevel[0] = matchCount;
+							}
+							if (matchCount == matchLevel[0]) {
+								resources.add(resource);
+							}
+						}
+
+						return true; // visit also members of this
+										// resource
+					}
+				});
+		} catch(CoreException e) {
+			StatusHandler.log(e.getStatus());
+			return null;
+		}
+		
+		if (resources.size() > 0) {
+			return resources.get(0);
+		}
+		
+		StatusHandler.log(new Status(IStatus.WARNING, AtlassianSubversiveCorePlugin.PLUGIN_ID, NLS.bind("Could not find resource for {0}.", path)));
 		return null;
 	}
 
