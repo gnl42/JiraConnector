ab634e3865cd138b4ce4f27c30ea77cc0ea34f57
Steffen Pingel <steffen.pingel@tasktop.com>
2010-01-05 22:04:17 +0000

    NEW - bug 277255: JiraClientOfflineTest failing https://bugs.eclipse.org/bugs/show_bug.cgi?id=277255

--- org.eclipse.mylyn.jira.tests/src/org/eclipse/mylyn/jira/tests/client/JiraClientOfflineTest.java
+++ org.eclipse.mylyn.jira.tests/src/org/eclipse/mylyn/jira/tests/client/JiraClientOfflineTest.java
@@ -17,6 +17,7 @@ import org.eclipse.mylyn.commons.net.WebLocation;
 import org.eclipse.mylyn.commons.tests.support.TestProxy;
 import org.eclipse.mylyn.internal.jira.core.model.JiraWorkLog;
 import org.eclipse.mylyn.internal.jira.core.service.JiraClient;
+import org.eclipse.mylyn.internal.jira.core.service.JiraServiceUnavailableException;
 import org.eclipse.mylyn.jira.tests.util.JiraTestUtil;
 
 /**
@@ -28,10 +29,15 @@ public class JiraClientOfflineTest extends TestCase {
 
 	private TestProxy server;
 
+	private String manglePort(String message) {
+		return message.replaceAll("http:\\/\\/localhost\\/", "http://localhost:" + server.getPort() + "/");
+	}
+
 	@Override
 	protected void setUp() throws Exception {
 		server = new TestProxy();
 		server.startAndWait();
+		server.setDebugEnabled(true);
 		client = new JiraClient(new WebLocation("http://localhost:" + server.getPort(), "", ""));
 	}
 
@@ -40,40 +46,42 @@ public class JiraClientOfflineTest extends TestCase {
 		server.stop();
 	}
 
-	public void DISABLEDtestBrowseIssueRedirect() throws Exception {
+	public void testBrowseIssueRedirect() throws Exception {
 		server.addResponse(JiraTestUtil.getMessage("web/login-success-response"));
 		server.addResponse(JiraTestUtil.getMessage("soap/login-success-response"));
 		server.addResponse(JiraTestUtil.getMessage("soap/get-server-info-3-13-1-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/login-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/logout-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/browse-issue-redirect-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/logout-success-response"));
+		server.addResponse(JiraTestUtil.getMessage("web/user-preferences-head-response"));
+		server.addResponse(manglePort(JiraTestUtil.getMessage("web/browse-issue-redirect-response")));
 		client.getIssueByKey("KEY-1", null);
 	}
 
-	public void DISABLEDtestHttpsRedirect() throws Exception {
-		server.addResponse(JiraTestUtil.getMessage("soap/login-redirect-response"));
-		client.getServerInfo(null);
+	public void testGetWorklogs() throws Exception {
+		server.addResponse(JiraTestUtil.getMessage("soap/login-success-response"));
+		server.addResponse(JiraTestUtil.getMessage("soap/get-server-info-3-13-1-success-response"));
+		server.addResponse(JiraTestUtil.getMessage("web/login-success-response"));
+		server.addResponse(JiraTestUtil.getMessage("soap/get-worklogs-two-entries-success-response"));
+		JiraWorkLog[] worklogs = client.getWorklogs("KEY-1", null);
+		assertNotNull(worklogs);
+		assertEquals(2, worklogs.length);
 	}
 
 	public void testGetWorklogsUnsupportedVersion() throws Exception {
 		server.addResponse(JiraTestUtil.getMessage("soap/login-success-response"));
 		server.addResponse(JiraTestUtil.getMessage("soap/get-server-info-3-6-2-success-response"));
 		server.addResponse(JiraTestUtil.getMessage("web/login-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/logout-success-response"));
+		server.addResponse(JiraTestUtil.getMessage("web/user-preferences-head-response"));
 		server.addResponse(JiraTestUtil.getMessage("soap/get-worklogs-no-such-operation-response"));
 		assertNull(client.getWorklogs("KEY-1", null));
 	}
 
-	public void DISABLEDtestGetWorklogs() throws Exception {
-		server.addResponse(JiraTestUtil.getMessage("soap/login-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("soap/get-server-info-3-13-1-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/login-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("web/logout-success-response"));
-		server.addResponse(JiraTestUtil.getMessage("soap/get-worklogs-two-entries-success-response"));
-		JiraWorkLog[] worklogs = client.getWorklogs("KEY-1", null);
-		assertNotNull(worklogs);
-		assertEquals(2, worklogs.length);
+	public void testHttpsRedirect() throws Exception {
+		client.getConfiguration().setFollowRedirects(false);
+		try {
+			server.addResponse(JiraTestUtil.getMessage("soap/login-redirect-response"));
+			client.getServerInfo(null);
+			fail("Expected JiraServiceUnavailableException");
+		} catch (JiraServiceUnavailableException e) {
+		}
 	}
 
 }
