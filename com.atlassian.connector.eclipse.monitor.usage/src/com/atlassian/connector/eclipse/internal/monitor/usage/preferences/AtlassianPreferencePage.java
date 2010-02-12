65de98a70baf4b738335f0cf89ddceb429418f9f
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2010-02-12 14:05:30 +0000

    PLE-958

---
+++ com.atlassian.connector.eclipse.monitor.usage/src/com/atlassian/connector/eclipse/internal/monitor/usage/preferences/AtlassianPreferencePage.java
@@ -0,0 +1,29 @@
+package com.atlassian.connector.eclipse.internal.monitor.usage.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class AtlassianPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	private static final String DESCRIPTION = "Expand the tree to edit preferences for a specific feature";
+
+	@Override
+	protected Control createContents(Composite parent) {
+		return null;
+	}
+
+	public AtlassianPreferencePage() {
+		noDefaultAndApplyButton();
+		setDescription(DESCRIPTION);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+
+}
