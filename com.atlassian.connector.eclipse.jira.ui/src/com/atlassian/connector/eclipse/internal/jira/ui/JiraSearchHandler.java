c8b379a3884bedff479bbea54c63b4b5bb8b6d12
Wojciech Seliga <wseliga@atlassian.com>
2010-04-01 08:37:07 +0000

    PLE-1053 fixing duplicate stack trace detection with JIRA 4+ (in progress)

--- com.atlassian.connector.eclipse.jira.ui/src/com/atlassian/connector/eclipse/internal/jira/ui/JiraSearchHandler.java
+++ com.atlassian.connector.eclipse.jira.ui/src/com/atlassian/connector/eclipse/internal/jira/ui/JiraSearchHandler.java
@@ -12,17 +12,17 @@
 
 package com.atlassian.connector.eclipse.internal.jira.ui;
 
-import java.util.StringTokenizer;
+import com.atlassian.connector.eclipse.internal.jira.core.JiraCorePlugin;
+import com.atlassian.connector.eclipse.internal.jira.core.model.filter.ContentFilter;
+import com.atlassian.connector.eclipse.internal.jira.core.model.filter.FilterDefinition;
+import com.atlassian.connector.eclipse.internal.jira.core.util.JiraUtil;
 
 import org.eclipse.mylyn.internal.tasks.core.AbstractSearchHandler;
 import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.core.data.TaskData;
 
-import com.atlassian.connector.eclipse.internal.jira.core.JiraCorePlugin;
-import com.atlassian.connector.eclipse.internal.jira.core.model.filter.ContentFilter;
-import com.atlassian.connector.eclipse.internal.jira.core.model.filter.FilterDefinition;
-import com.atlassian.connector.eclipse.internal.jira.core.util.JiraUtil;
+import java.util.StringTokenizer;
 
 /**
  * Stack Trace duplicate detector
@@ -43,33 +43,35 @@ public class JiraSearchHandler extends AbstractSearchHandler {
 	@Override
 	public boolean queryForText(TaskRepository taskRepository, IRepositoryQuery query, TaskData taskData,
 			String searchString) {
-		StringBuilder sb = new StringBuilder(MAX_LENGTH);
-		if (searchString.length() > MAX_LENGTH) {
-			// searching for exact matches fails if strings are too long
-			StringTokenizer t = new StringTokenizer(searchString, " \n\t()"); //$NON-NLS-1$
-			while (t.hasMoreTokens() && sb.length() < MAX_LENGTH - 20) {
-				if (sb.length() > 0) {
-					sb.append(" AND "); //$NON-NLS-1$
-				}
-				int remaining = MAX_LENGTH - sb.length();
-				String token = t.nextToken();
-				if (token.length() > remaining) {
-					sb.append(token.substring(0, remaining));
-					sb.append("*"); //$NON-NLS-1$
-				} else {
-					sb.append(token);
-				}
-			}
-		} else {
-			sb.append("\""); //$NON-NLS-1$
-			sb.append(searchString);
-			sb.append("\""); //$NON-NLS-1$
-		}
-
+		final String preparedSearchString = prepareSearchString(searchString);
 		FilterDefinition filter = new FilterDefinition();
-		filter.setContentFilter(new ContentFilter(sb.toString(), false, true, false, true));
+		filter.setContentFilter(new ContentFilter(preparedSearchString, false, true, false, true));
 		JiraUtil.setQuery(taskRepository, query, filter);
 		return true;
+	}
+
+	public static String prepareSearchString(String searchString) {
+		StringBuilder sb = new StringBuilder(MAX_LENGTH);
+		// on JIRA 4+ you cannot search for :
+		// on older JIRAs your search fails if the query is too long
+		// because I don't have a progress monitor here, I can not easily fetch JIRA version (it may be not yet cached)
+		// so I assume the worst and apply the solution (not perfect but good enough) which should
+		// work with all supported JIRA versions
+		StringTokenizer t = new StringTokenizer(searchString, " :\n\t()"); //$NON-NLS-1$
+		while (t.hasMoreTokens() && sb.length() < MAX_LENGTH - 20) {
+			if (sb.length() > 0) {
+				sb.append(" AND "); //$NON-NLS-1$
+			}
+			int remaining = MAX_LENGTH - sb.length();
+			String token = t.nextToken();
+			if (token.length() > remaining) {
+				sb.append(token.substring(0, remaining));
+				sb.append("*"); //$NON-NLS-1$
+			} else {
+				sb.append(token);
+			}
+		}
+		return sb.toString();
 	}
 
 }
