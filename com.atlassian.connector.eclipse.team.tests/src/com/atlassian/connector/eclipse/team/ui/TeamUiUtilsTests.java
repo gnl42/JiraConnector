9325f0d01f6e61e633b6085d767355a5b85939bd
Wojciech Seliga <wseliga@atlassian.com>
2010-04-07 12:14:08 +0000

    NONE making test less fragile when cleanup was not correctly called

--- com.atlassian.connector.eclipse.team.tests/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtilsTests.java
+++ com.atlassian.connector.eclipse.team.tests/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtilsTests.java
@@ -86,6 +86,7 @@ public final class TeamUiUtilsTests extends TestCase {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		cleanupWorkspace();
 		project = new TestJavaProject(this.getClass().getSimpleName());
 		nonJavaProject = new TestProject(this.getClass().getSimpleName() + "nonJava");
 
@@ -93,11 +94,15 @@ public final class TeamUiUtilsTests extends TestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
+		cleanupWorkspace();
+		super.tearDown();
+	}
+
+	private void cleanupWorkspace() throws CoreException {
 		ResourceTestUtil.deleteProject(project.getProject());
 		ResourceTestUtil.deleteProject(nonJavaProject.getProject());
 
 		AbstractJavaContextTest.waitForAutoBuild();
-		super.tearDown();
 	}
 
 	public void testGetSupportedTeamConnectors() {
