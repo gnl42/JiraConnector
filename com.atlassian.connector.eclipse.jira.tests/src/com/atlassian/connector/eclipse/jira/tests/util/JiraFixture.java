54da61fa6d36eb8bfd9ef8f45f15a49f6a9d6a39
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2010-05-19 08:32:39 +0000

    NONE: JiraFixture

--- com.atlassian.connector.eclipse.jira.tests/src/com/atlassian/connector/eclipse/jira/tests/util/JiraFixture.java
+++ com.atlassian.connector.eclipse.jira.tests/src/com/atlassian/connector/eclipse/jira/tests/util/JiraFixture.java
@@ -40,7 +40,7 @@ public class JiraFixture extends TestFixture {
 			"4.1.1", "522", "Enterprise");
 
 	public static JiraFixture SNAPSHOT = new JiraFixture(getServerUrl("jira-enterprise-snapshot"), //
-			"4.2", "551", "Enterprise");
+			"4.2-SNAPSHOT", "551", "Enterprise");
 
 	public static final JiraFixture[] ALL = new JiraFixture[] { ENTERPRISE_3_13, ENTERPRISE_4_0, SNAPSHOT };
 
