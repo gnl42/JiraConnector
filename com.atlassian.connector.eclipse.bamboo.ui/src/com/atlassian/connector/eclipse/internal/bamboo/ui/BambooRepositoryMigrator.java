9284a6cb598b5185a094c20b3dbedc6e4e9ab20c
Wojciech Seliga <wseliga@atlassian.com>
2010-05-31 14:55:47 +0000

    PLE-1147

---
+++ com.atlassian.connector.eclipse.bamboo.ui/src/com/atlassian/connector/eclipse/internal/bamboo/ui/BambooRepositoryMigrator.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Atlassian and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Atlassian - initial API and implementation
+ ******************************************************************************/
+
+package com.atlassian.connector.eclipse.internal.bamboo.ui;
+
+import com.atlassian.connector.eclipse.commons.internal.ui.CategoryAddingRepositoryMigrator;
+import com.atlassian.connector.eclipse.internal.bamboo.core.BambooCorePlugin;
+
+import org.eclipse.mylyn.internal.tasks.core.IRepositoryConstants;
+
+public class BambooRepositoryMigrator extends CategoryAddingRepositoryMigrator {
+	public BambooRepositoryMigrator() {
+		super(BambooCorePlugin.CONNECTOR_KIND, IRepositoryConstants.CATEGORY_BUILD);
+	}
+}
