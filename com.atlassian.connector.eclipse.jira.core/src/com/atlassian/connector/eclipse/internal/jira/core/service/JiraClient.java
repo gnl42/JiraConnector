ab1b502c76f14ceb1eb1f5a1e6b78aeb9ad2187e
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2010-07-23 08:40:08 +0000

    PLE-1188: deleteIssue uses webClient

--- com.atlassian.connector.eclipse.jira.core/src/com/atlassian/connector/eclipse/internal/jira/core/service/JiraClient.java
+++ com.atlassian.connector.eclipse.jira.core/src/com/atlassian/connector/eclipse/internal/jira/core/service/JiraClient.java
@@ -208,7 +208,8 @@ public class JiraClient {
 	}
 
 	public void deleteIssue(JiraIssue issue, IProgressMonitor monitor) throws JiraException {
-		soapClient.deleteIssue(issue.getKey(), monitor);
+		//soapClient.deleteIssue(issue.getKey(), monitor);
+		webClient.deleteIssue(issue, monitor);
 	}
 
 	@Override
