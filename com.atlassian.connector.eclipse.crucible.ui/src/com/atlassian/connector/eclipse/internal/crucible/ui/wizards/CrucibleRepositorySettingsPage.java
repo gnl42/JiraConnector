eb376a9f3e7d2188201d0bc5938470774c6d73f1
Wojciech Seliga <wseliga@atlassian.com>
2010-08-09 13:54:12 +0000

    ACC-75 fixing captcha in Eclipse

--- com.atlassian.connector.eclipse.crucible.ui/src/com/atlassian/connector/eclipse/internal/crucible/ui/wizards/CrucibleRepositorySettingsPage.java
+++ com.atlassian.connector.eclipse.crucible.ui/src/com/atlassian/connector/eclipse/internal/crucible/ui/wizards/CrucibleRepositorySettingsPage.java
@@ -21,8 +21,8 @@ import com.atlassian.connector.eclipse.internal.crucible.core.client.CrucibleCli
 import com.atlassian.connector.eclipse.internal.crucible.ui.CrucibleUiPlugin;
 import com.atlassian.connector.eclipse.internal.fisheye.core.client.FishEyeClient;
 import com.atlassian.connector.eclipse.internal.fisheye.core.client.FishEyeClientData;
+import com.atlassian.theplugin.commons.remoteapi.CaptchaRequiredException;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -39,8 +39,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
-
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -75,15 +73,12 @@ public class CrucibleRepositorySettingsPage extends AbstractRepositorySettingsPa
 				client.validate(monitor, taskRepository);
 			} catch (CoreException e) {
 				IStatus status = e.getStatus();
-				if (e.getCause() != null && e.getCause() instanceof RemoteApiException
-						&& e.getCause().getCause() != null && e.getCause().getCause() instanceof IOException) {
-					if (e.getCause().getCause().getMessage().contains("HTTP 404")) {
+				if (e.getCause() != null && e.getCause() instanceof RemoteApiException) {
+					if (e.getMessage().contains("HTTP 404")) {
 						status = new Status(IStatus.ERROR, CrucibleUiPlugin.PLUGIN_ID,
 								"HTTP 404 (Not Found) - Did you enable Remote API in Crucible?", e);
 					}
-					if (e.getCause().getCause().getMessage().contains("HTTP 403")
-							&& e.getCause().getCause().getCause() != null
-							&& e.getCause().getCause().getCause().getMessage().contains("maximum")) {
+					if (e.getCause() instanceof CaptchaRequiredException) {
 						Display.getDefault().asyncExec(new Runnable() {
 							public void run() {
 								new RemoteApiLockedDialog(WorkbenchUtil.getShell(), taskRepository.getRepositoryUrl()).open();
