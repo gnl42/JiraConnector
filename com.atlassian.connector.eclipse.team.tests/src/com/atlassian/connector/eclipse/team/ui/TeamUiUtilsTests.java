132cbb9e318e4565f1445b6631cf88885ea5b4e4
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2010-09-16 14:49:46 +0200

    PLE-1230

--- com.atlassian.connector.eclipse.team.tests/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtilsTests.java
+++ com.atlassian.connector.eclipse.team.tests/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtilsTests.java
@@ -11,21 +11,10 @@
 
 package com.atlassian.connector.eclipse.team.ui;
 
-import static org.mockito.Mockito.when;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IType;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
-
-import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
 
 import junit.framework.TestCase;
 
@@ -34,53 +23,6 @@ public final class TeamUiUtilsTests extends TestCase {
 	private TestJavaProject project;
 
 	private TestProject nonJavaProject;
-
-	public void testFindResourceForPath() throws CoreException, UnsupportedEncodingException {
-		final IPackageFragment p1 = project.createPackage("p1");
-		final IType type1 = project.createType(p1, "Type1.java", "public class Type1 { }");
-		final IType type2 = project.createType(p1, "Type2.java", "public class Type2 { }");
-		final IFolder metaInfFolder = nonJavaProject.getProject().getFolder("META-INF");
-		metaInfFolder.create(true, true, null);
-		final IFile manifestFile = metaInfFolder.getFile("MANIFEST.MF");
-		ByteArrayInputStream in = new ByteArrayInputStream("ABC".getBytes("UTF-8"));
-		manifestFile.create(in, true, null);
-
-		TeamUiResourceManager teamResourceManager = AtlassianTeamUiPlugin.getDefault().getTeamResourceManager();
-		final ITeamUiResourceConnector mockConnector = Mockito.mock(ITeamUiResourceConnector.class);
-		when(mockConnector.canHandleFile(Matchers.<IFile> anyObject())).thenReturn(true);
-		when(mockConnector.isEnabled()).thenReturn(true);
-		ScmRepository javaProjectScmRepository = new ScmRepository("http://localhost/svn2/trunk",
-				"http://localhost/svn2", "svnrepo2", mockConnector);
-		ScmRepository nonjavaProjectScmRepository = new ScmRepository("http://localhost/svn/trunk",
-				"http://localhost/svn", "svnrepo", mockConnector);
-		when(mockConnector.getApplicableRepository(project.getProject())).thenReturn(javaProjectScmRepository);
-		when(mockConnector.getApplicableRepository(nonJavaProject.getProject())).thenReturn(nonjavaProjectScmRepository);
-
-		teamResourceManager.addTeamConnector(mockConnector);
-
-		assertNull(TeamUiUtils.findResourceForPath("http://localhost/svn", "trunk/src/a/b/A.java", null));
-		assertEquals(manifestFile, TeamUiUtils.findResourceForPath("http://localhost/svn",
-				"trunk/non-java-project/META-INF/MANIFEST.MF", null));
-
-		assertNull(TeamUiUtils.findResourceForPath("http://localhost2/svn",
-				"trunk/non-java-project/META-INF/MANIFEST.MF", null));
-
-		assertEquals(manifestFile, TeamUiUtils.findResourceForPath("http://localhost/svn",
-				"trunk/non-java-project/META-INF/MANIFEST.MF", null));
-
-		assertEquals(null, TeamUiUtils.findResourceForPath("http://localhost/svn2",
-				"trunk/some-java-project/p1/Type1.java", null));
-		assertEquals(type1.getResource(), TeamUiUtils.findResourceForPath("http://localhost/svn2",
-				"trunk/some-java-project/src/p1/Type1.java", null));
-		assertEquals(type2.getResource(), TeamUiUtils.findResourceForPath("http://localhost/svn2",
-				"trunk/some-java-project/src/p1/Type2.java", null));
-		assertEquals(type1.getResource(), TeamUiUtils.findResourceForPath("http://localhost/svn2",
-				"branches/r1-1/some-java-project/src/p1/Type1.java", null));
-
-		assertEquals(null, TeamUiUtils.findResourceForPath("http://localhost/svn",
-				"trunk/some-java-project/src/p1/Type2.java", null));
-
-	}
 
 	@Override
 	protected void setUp() throws Exception {
--- com.atlassian.connector.eclipse.team.ui/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtils.java
+++ com.atlassian.connector.eclipse.team.ui/src/com/atlassian/connector/eclipse/team/ui/TeamUiUtils.java
@@ -11,29 +11,18 @@
 
 package com.atlassian.connector.eclipse.team.ui;
 
-import com.atlassian.connector.eclipse.ui.commons.DecoratedResource;
 import com.atlassian.theplugin.commons.util.MiscUtil;
 
-import org.eclipse.compare.CompareEditorInput;
-import org.eclipse.compare.CompareUI;
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.mylyn.commons.core.StatusHandler;
 import org.eclipse.mylyn.internal.provisional.commons.ui.WorkbenchUtil;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -57,33 +46,6 @@ public final class TeamUiUtils {
 	private TeamUiUtils() {
 	}
 
-	public static DefaultTeamUiResourceConnector getDefaultConnector() {
-		return defaultConnector;
-	}
-
-	public static boolean isInSync(IResource resource, String revision) {
-		LocalStatus status;
-		try {
-			status = getLocalRevision(resource);
-			return (status != null && !status.isDirty() && (status.getRevision().equals(revision) || status.getLastChangedRevision()
-					.equals(revision)));
-		} catch (CoreException e) {
-			return false;
-		}
-	}
-
-	public static Collection<String> getSupportedTeamConnectors() {
-		Collection<String> res = MiscUtil.buildArrayList();
-		TeamUiResourceManager teamResourceManager = AtlassianTeamUiPlugin.getDefault().getTeamResourceManager();
-		for (ITeamUiResourceConnector connector : teamResourceManager.getTeamConnectors()) {
-			if (connector.isEnabled()) {
-				res.add(connector.getName());
-			}
-		}
-		res.add(defaultConnector.getName());
-		return res;
-	}
-
 	public static boolean hasNoTeamConnectors() {
 		return AtlassianTeamUiPlugin.getDefault().getTeamResourceManager().getTeamConnectors().size() == 0;
 	}
@@ -94,55 +56,6 @@ public final class TeamUiUtils {
 			return false;
 		}
 		return true;
-	}
-
-	@Nullable
-	public static IResource findResourceForPath(String repoUrl, String filePath, IProgressMonitor monitor) {
-		IPath path = new Path(filePath);
-		final IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(path);
-		if (resource == null && repoUrl != null) {
-			for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
-				final ScmRepository scmRepo = getApplicableRepository(project);
-				if (scmRepo != null) {
-					if (repoUrl.startsWith(scmRepo.getRootPath())) {
-						final IResource member = findResourceForPath2(project, filePath);
-						if (member != null) {
-							return member;
-						}
-					}
-				}
-			}
-		}
-
-		if (resource == null) {
-			return findResourceForPath2(ResourcesPlugin.getWorkspace().getRoot(), filePath);
-		}
-		return resource;
-	}
-
-	public static String findMatching(String projectScmPath, String filePath) {
-		return null;
-	}
-
-	private static IResource findResourceForPath2(IContainer location, String filePath) {
-		if (filePath == null || filePath.length() <= 0) {
-			return null;
-		}
-
-		IPath path = new Path(filePath);
-		IResource resource = null;
-		while (!path.isEmpty() && resource == null) {
-			resource = match(location, path);
-			path = path.removeFirstSegments(1);
-		}
-		return resource;
-	}
-
-	private static IResource match(IContainer location, IPath path) {
-		if (!path.isEmpty()) {
-			return location.findMember(path);
-		}
-		return null;
 	}
 
 	@Nullable
@@ -180,24 +93,6 @@ public final class TeamUiUtils {
 		return null;
 	}
 
-	public static void openCompareEditorForInput(final CompareEditorInput compareEditorInput) {
-		if (Display.getCurrent() != null) {
-			internalOpenCompareEditorForInput(compareEditorInput);
-		} else {
-			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-				public void run() {
-					internalOpenCompareEditorForInput(compareEditorInput);
-				}
-			});
-		}
-	}
-
-	private static void internalOpenCompareEditorForInput(CompareEditorInput compareEditorInput) {
-		IWorkbench workbench = AtlassianTeamUiPlugin.getDefault().getWorkbench();
-		IWorkbenchPage page = workbench.getActiveWorkbenchWindow().getActivePage();
-		CompareUI.openCompareEditorOnPage(compareEditorInput, page);
-	}
-
 	public static void handleMissingTeamConnectors() {
 		Display.getDefault().syncExec(new Runnable() {
 			public void run() {
@@ -231,51 +126,6 @@ public final class TeamUiUtils {
 		res.addAll(defaultConnector.getRepositories(monitor));
 		return res;
 
-	}
-
-	public static DecoratedResource getDecoratedResource(IResource resource, ITeamUiResourceConnector connector) {
-
-		final String PRE_COMMIT_EXPLANATION = " and will be added to the review in pre-commit mode.";
-
-		if (connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_UNVERSIONED)) {
-			return new DecoratedResource(resource, false, "pre-commit", "This file is unversioned"
-					+ PRE_COMMIT_EXPLANATION);
-		} else if (connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_IGNORED)) {
-			return new DecoratedResource(resource, false, "pre-commit", "This file is ignored in version control"
-					+ PRE_COMMIT_EXPLANATION);
-		} else if (connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_ANY_CHANGE)) {
-			return new DecoratedResource(resource, false, "pre-commit", "This file has been added or changed locally"
-					+ PRE_COMMIT_EXPLANATION);
-		} else if (connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_VERSIONED)) {
-			return new DecoratedResource(resource, true, "", "This file is up to date.");
-		} else {
-			// ignore the resource
-		}
-
-		return null;
-	}
-
-	public static String getScmPath(IResource resource, ITeamUiResourceConnector connector) {
-		if (connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_VERSIONED)
-				&& !connector.isResourceAcceptedByFilter(resource, ITeamUiResourceConnector.State.SF_ANY_CHANGE)) {
-			try {
-				LocalStatus status = connector.getLocalRevision(resource);
-				if (status.getScmPath() != null && status.getScmPath().length() > 0) {
-					LocalStatus projectStatus = connector.getLocalRevision(resource.getProject());
-					return projectStatus != null && projectStatus.getScmPath() != null ? projectStatus.getScmPath()
-							: status.getScmPath();
-				}
-			} catch (CoreException e) {
-				// resource is probably not under version control
-				// skip
-			}
-		}
-
-		return null;
-	}
-
-	public static ITeamUiResourceConnector getTeamConnector(IResource resource) {
-		return AtlassianTeamUiPlugin.getDefault().getTeamResourceManager().getTeamConnector(resource);
 	}
 
 }
