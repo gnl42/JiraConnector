20e35cc2aa6694f7f90f1a15276bf72519abf798
Paweł Niewiadomski <pniewiadomski@atlassian.com>
2012-01-18 11:57:16 +0000

    JRJC-40: fixed integration test
    git-svn-id: https://studio.atlassian.com/svn/JRJC/trunk@133876 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-jira-rest-java-client/src/test/java/com/atlassian/jira/rest/client/IntegrationTestUtil.java
+++ atlassian-jira-rest-java-client/src/test/java/com/atlassian/jira/rest/client/IntegrationTestUtil.java
@@ -31,6 +31,8 @@ public class IntegrationTestUtil {
 	public static final BasicUser USER_ADMIN_LATEST;
     public static final BasicUser USER1;
     public static final BasicUser USER2;
+	public static final BasicUser USER1_LATEST;
+    public static final BasicUser USER2_LATEST;
 	public static final BasicUser USER_SLASH;
 	public static final BasicUser USER_SLASH_LATEST;
 
@@ -53,6 +55,8 @@ public class IntegrationTestUtil {
 
             USER1 = new BasicUser(getUserUri("wseliga"), "wseliga", "Wojciech Seliga");
             USER2 = new BasicUser(getUserUri("user"), "user", "My Test User");
+			USER1_LATEST = new BasicUser(getLatestUserUri("wseliga"), "wseliga", "Wojciech Seliga");
+            USER2_LATEST = new BasicUser(getLatestUserUri("user"), "user", "My Test User");
 			USER_SLASH = new BasicUser(getUserUri("a/user/with/slash"), "a/user/with/slash", "A User with / in its username");
 			USER_SLASH_LATEST = new BasicUser(getLatestUserUri("a/user/with/slash"), "a/user/with/slash", "A User with / in its username");
             USER_ADMIN = new BasicUser(getUserUri("admin"), "admin", "Administrator");
