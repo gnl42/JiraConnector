e75d20f8a14014bc46d5a75f1a87a85f73cb8344
Jacek Jaroczynski <jjaroczynski@atlassian.com>
2013-01-08 14:23:07 +0100

    debug for CI failing tests (work fine locally)

--- com.atlassian.connector.eclipse.jira.core/src/com/atlassian/connector/eclipse/internal/jira/core/service/rest/JiraRestConverter.java
+++ com.atlassian.connector.eclipse.jira.core/src/com/atlassian/connector/eclipse/internal/jira/core/service/rest/JiraRestConverter.java
@@ -154,6 +154,9 @@ public class JiraRestConverter {
 		}
 
 		// TODO rest: do we need to use cache here? can't we create priority and other objects from issue?
+		cache.toString();
+		issue.getPriority().toString();
+		issue.getPriority().getName().toString();
 		jiraIssue.setPriority(cache.getPriorityByName(issue.getPriority().getName()));
 		jiraIssue.setStatus(cache.getStatusByName(issue.getStatus().getName()));
 
