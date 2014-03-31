97beafb6d6e9be8816606504029f668708c21cf5
Michal Orzechowski <morzechowski@atlassian.com>
2014-03-31 13:40:48 +0200

    JDEV-27955: add hashcode and equals to audit records data

--- api/src/main/java/com/atlassian/jira/rest/client/api/domain/AuditRecordsData.java
+++ api/src/main/java/com/atlassian/jira/rest/client/api/domain/AuditRecordsData.java
@@ -1,5 +1,7 @@
 package com.atlassian.jira.rest.client.api.domain;
 
+import com.google.common.base.Objects;
+
 /**
  * Represents audit search metadata and audit result records
  */
@@ -33,5 +35,28 @@ public class AuditRecordsData {
         return records;
     }
 
-    //todo - add missing methods: equals, hashcode ...
+    protected Objects.ToStringHelper getToStringHelper() {
+        return Objects.toStringHelper(this).
+                add("offset", offset).
+                add("limit", limit).
+                add("total", total).
+                add("records", records);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o instanceof AuditRecordsData) {
+            final AuditRecordsData that = (AuditRecordsData) o;
+            return Objects.equal(this.offset, that.offset)
+                    && Objects.equal(this.limit, that.limit)
+                    && Objects.equal(this.total, that.total)
+                    && Objects.equal(this.records, that.records);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(offset, limit, total, records);
+    }
 }
