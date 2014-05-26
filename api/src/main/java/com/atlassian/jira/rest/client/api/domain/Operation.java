160bff3959d083811f1f16ac4f5ec4b3731559b3
Cezary Zawadka <czawadka@atlassian.com>
2014-05-26 10:43:39 +0200

    JRJC-182: add javadocs, license information

--- api/src/main/java/com/atlassian/jira/rest/client/api/domain/Operation.java
+++ api/src/main/java/com/atlassian/jira/rest/client/api/domain/Operation.java
@@ -1,10 +1,37 @@
+/*
+ * Copyright (C) 2014 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.atlassian.jira.rest.client.api.domain;
 
 import javax.annotation.Nullable;
 
 public interface Operation {
+	/**
+	 * @return Operation id. May be null.
+	 */
 	@Nullable String getId();
 
-	<T> T accept(OperationVisitor<T> visitor);
+	/**
+	 * Traverse the operation elements to visit them. Traversal will stop on first non null value returned from
+	 * the visitor.
+	 *
+	 * @param visitor Visitor to visit operation element
+	 * @param <T> Visiting result type
+	 * @return Value returned from visitor.
+	 */
+	@Nullable <T> T accept(OperationVisitor<T> visitor);
 
 }
--- api/src/main/java/com/atlassian/jira/rest/client/api/domain/Operations.java
+++ api/src/main/java/com/atlassian/jira/rest/client/api/domain/Operations.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright (C) 2014 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.atlassian.jira.rest.client.api.domain;
 
 import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.google.common.base.Objects;
 import com.google.common.collect.Iterables;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 
 /**
@@ -22,10 +39,12 @@ public class Operations {
 		return linkGroups;
 	}
 
+	@Nullable
 	public <T> T accept(OperationVisitor<T> visitor) {
 		return OperationGroup.accept(getLinkGroups(), visitor);
 	}
 
+	@Nullable
 	public Operation getOperationById(final String operationId) {
 		return accept(new OperationVisitor<Operation>() {
 			@Override
