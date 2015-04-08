a0b8ccec77ae880e625df7c9017f097c48c06599
Dariusz Szuksztul <dszuksztul@atlassian.com>
2015-04-08 22:38:04 +0200

    LEGO-857 - replace contains with hasItem that is appropriate in this context

--- test/src/test/java/it/AsynchronousAuditRestClientTest.java
+++ test/src/test/java/it/AsynchronousAuditRestClientTest.java
@@ -202,8 +202,7 @@ public class AsynchronousAuditRestClientTest  extends AbstractAsynchronousRestCl
         final AuditRecordsData auditRecordsData = client.getAuditRestClient().getAuditRecords(fromLatestSearchCriteria).claim();
 
         // then
-        assertThat(auditRecordsData.getRecords(), Matchers.contains(auditRecordWithId(latestCreatedRecord.getId())));
-        assertThat(Iterables.size(auditRecordsData.getRecords()), is(Matchers.greaterThanOrEqualTo(1)));
+        assertThat(auditRecordsData.getRecords(), Matchers.hasItem(auditRecordWithId(latestCreatedRecord.getId())));
     }
 
     private Matcher<AuditRecord> auditRecordWithId(final Long expectedId) {
