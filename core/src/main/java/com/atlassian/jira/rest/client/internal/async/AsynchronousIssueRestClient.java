6848f495828027f90872c7ab602732ca4a6b823a
Richard Atkins <richatkins@atlassian.com>
2017-11-28 12:38:04 +1100

    CONFSRVDEV-3933 Windows portability fixes
     * Do not use platform default charset. Not even once
     * Refactor to stop using deprecated httpclient MultipartEntity, and use MultipartEntityBuilder instead
     * Replace guava Function with Java Function, and use streams and lambdas where possible

--- core/src/main/java/com/atlassian/jira/rest/client/internal/async/AsynchronousIssueRestClient.java
+++ core/src/main/java/com/atlassian/jira/rest/client/internal/async/AsynchronousIssueRestClient.java
@@ -17,23 +17,51 @@ package com.atlassian.jira.rest.client.internal.async;
 
 import com.atlassian.httpclient.apache.httpcomponents.MultiPartEntityBuilder;
 import com.atlassian.httpclient.api.HttpClient;
-import com.atlassian.httpclient.api.Response;
+import com.atlassian.httpclient.api.Message;
 import com.atlassian.httpclient.api.ResponsePromise;
-import com.atlassian.jira.rest.client.api.*;
-import com.atlassian.jira.rest.client.api.domain.*;
-import com.atlassian.jira.rest.client.api.domain.input.*;
+import com.atlassian.jira.rest.client.api.GetCreateIssueMetadataOptions;
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.MetadataRestClient;
+import com.atlassian.jira.rest.client.api.RestClientException;
+import com.atlassian.jira.rest.client.api.SessionRestClient;
+import com.atlassian.jira.rest.client.api.domain.BasicIssue;
+import com.atlassian.jira.rest.client.api.domain.BulkOperationResult;
+import com.atlassian.jira.rest.client.api.domain.CimProject;
+import com.atlassian.jira.rest.client.api.domain.Comment;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.ServerInfo;
+import com.atlassian.jira.rest.client.api.domain.Transition;
+import com.atlassian.jira.rest.client.api.domain.Votes;
+import com.atlassian.jira.rest.client.api.domain.Watchers;
+import com.atlassian.jira.rest.client.api.domain.input.AttachmentInput;
+import com.atlassian.jira.rest.client.api.domain.input.FieldInput;
+import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
+import com.atlassian.jira.rest.client.api.domain.input.LinkIssuesInput;
+import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
+import com.atlassian.jira.rest.client.api.domain.input.WorklogInput;
 import com.atlassian.jira.rest.client.internal.ServerVersionConstants;
-import com.atlassian.jira.rest.client.internal.json.*;
-import com.atlassian.jira.rest.client.internal.json.gen.*;
+import com.atlassian.jira.rest.client.internal.json.BasicIssueJsonParser;
+import com.atlassian.jira.rest.client.internal.json.BasicIssuesJsonParser;
+import com.atlassian.jira.rest.client.internal.json.CreateIssueMetadataJsonParser;
+import com.atlassian.jira.rest.client.internal.json.IssueJsonParser;
+import com.atlassian.jira.rest.client.internal.json.JsonObjectParser;
+import com.atlassian.jira.rest.client.internal.json.JsonParseUtil;
+import com.atlassian.jira.rest.client.internal.json.TransitionJsonParser;
+import com.atlassian.jira.rest.client.internal.json.TransitionJsonParserV5;
+import com.atlassian.jira.rest.client.internal.json.VotesJsonParser;
+import com.atlassian.jira.rest.client.internal.json.WatchersJsonParserBuilder;
+import com.atlassian.jira.rest.client.internal.json.gen.CommentJsonGenerator;
+import com.atlassian.jira.rest.client.internal.json.gen.IssueInputJsonGenerator;
+import com.atlassian.jira.rest.client.internal.json.gen.IssueUpdateJsonGenerator;
+import com.atlassian.jira.rest.client.internal.json.gen.IssuesInputJsonGenerator;
+import com.atlassian.jira.rest.client.internal.json.gen.LinkIssuesInputGenerator;
+import com.atlassian.jira.rest.client.internal.json.gen.WorklogInputJsonGenerator;
 import com.atlassian.util.concurrent.Promise;
-import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -41,11 +69,19 @@ import org.codehaus.jettison.json.JSONObject;
 import javax.annotation.Nullable;
 import javax.ws.rs.core.UriBuilder;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.charset.Charset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyList;
+import static org.apache.http.entity.ContentType.DEFAULT_BINARY;
 
 /**
  * Asynchronous implementation of IssueRestClient.
@@ -55,12 +91,7 @@ import java.util.*;
 public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient implements IssueRestClient {
 
 	private static final EnumSet<Expandos> DEFAULT_EXPANDS = EnumSet.of(Expandos.NAMES, Expandos.SCHEMA, Expandos.TRANSITIONS);
-	private static final Function<IssueRestClient.Expandos, String> EXPANDO_TO_PARAM = new Function<Expandos, String>() {
-		@Override
-		public String apply(Expandos from) {
-			return from.name().toLowerCase();
-		}
-	};
+	private static final Function<IssueRestClient.Expandos, String> EXPANDO_TO_PARAM = from -> from.name().toLowerCase();
 	private final SessionRestClient sessionRestClient;
 	private final MetadataRestClient metadataRestClient;
 
@@ -144,7 +175,7 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 
 	@Override
 	public Promise<Issue> getIssue(final String issueKey) {
-		return getIssue(issueKey, Collections.<Expandos>emptyList());
+		return getIssue(issueKey, emptyList());
 	}
 
 	@Override
@@ -152,7 +183,7 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 		final UriBuilder uriBuilder = UriBuilder.fromUri(baseUri);
 		final Iterable<Expandos> expands = Iterables.concat(DEFAULT_EXPANDS, expand);
 		uriBuilder.path("issue").path(issueKey).queryParam("expand",
-				Joiner.on(',').join(Iterables.transform(expands, EXPANDO_TO_PARAM)));
+				StreamSupport.stream(expands.spliterator(), false).map(EXPANDO_TO_PARAM).collect(Collectors.joining(",")));
 		return getAndParse(uriBuilder.build(), issueParser);
 	}
 
@@ -175,33 +206,30 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 	@Override
 	public Promise<Iterable<Transition>> getTransitions(final URI transitionsUri) {
 		return callAndParse(client().newRequest(transitionsUri).get(),
-				new AbstractAsynchronousRestClient.ResponseHandler<Iterable<Transition>>() {
-					@Override
-					public Iterable<Transition> handle(Response response) throws JSONException, IOException {
-						final JSONObject jsonObject = new JSONObject(response.getEntity());
-						if (jsonObject.has("transitions")) {
-							return JsonParseUtil.parseJsonArray(jsonObject.getJSONArray("transitions"), transitionJsonParserV5);
-						} else {
-							final Collection<Transition> transitions = new ArrayList<Transition>(jsonObject.length());
-							@SuppressWarnings("unchecked")
-							final Iterator<String> iterator = jsonObject.keys();
-							while (iterator.hasNext()) {
-								final String key = iterator.next();
-								try {
-									final int id = Integer.parseInt(key);
-									final Transition transition = transitionJsonParser.parse(jsonObject.getJSONObject(key), id);
-									transitions.add(transition);
-								} catch (JSONException e) {
-									throw new RestClientException(e);
-								} catch (NumberFormatException e) {
-									throw new RestClientException(
-											"Transition id should be an integer, but found [" + key + "]", e);
-								}
-							}
-							return transitions;
-						}
-					}
-				}
+				(ResponseHandler<Iterable<Transition>>) response -> {
+                    final JSONObject jsonObject = new JSONObject(response.getEntity());
+                    if (jsonObject.has("transitions")) {
+                        return JsonParseUtil.parseJsonArray(jsonObject.getJSONArray("transitions"), transitionJsonParserV5);
+                    } else {
+                        final Collection<Transition> transitions = new ArrayList<>(jsonObject.length());
+                        @SuppressWarnings("unchecked")
+                        final Iterator<String> iterator = jsonObject.keys();
+                        while (iterator.hasNext()) {
+                            final String key = iterator.next();
+                            try {
+                                final int id = Integer.parseInt(key);
+                                final Transition transition = transitionJsonParser.parse(jsonObject.getJSONObject(key), id);
+                                transitions.add(transition);
+                            } catch (JSONException e) {
+                                throw new RestClientException(e);
+                            } catch (NumberFormatException e) {
+                                throw new RestClientException(
+                                        "Transition id should be an integer, but found [" + key + "]", e);
+                            }
+                        }
+                        return transitions;
+                    }
+                }
 		);
 	}
 
@@ -305,27 +333,41 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 
 	@Override
 	public Promise<Void> addAttachment(final URI attachmentsUri, final InputStream inputStream, final String filename) {
-		final MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, null, Charset.defaultCharset());
-		entity.addPart(FILE_BODY_TYPE, new InputStreamBody(inputStream, filename));
-		return postAttachments(attachmentsUri, entity);
+		final MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create()
+				.setLaxMode()
+				.setCharset(UTF_8)
+				.addBinaryBody(
+						FILE_BODY_TYPE,
+						inputStream,
+						DEFAULT_BINARY,
+						filename);
+		return postAttachments(attachmentsUri, entityBuilder);
 	}
 
 	@Override
 	public Promise<Void> addAttachments(final URI attachmentsUri, final AttachmentInput... attachments) {
-		final MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, null, Charset.defaultCharset());
+		final MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create()
+				.setLaxMode()
+				.setCharset(UTF_8);
 		for (final AttachmentInput attachmentInput : attachments) {
-			entity.addPart(FILE_BODY_TYPE, new InputStreamBody(attachmentInput.getInputStream(), attachmentInput.getFilename()));
+			entityBuilder.addBinaryBody(
+					FILE_BODY_TYPE,
+					attachmentInput.getInputStream(),
+					DEFAULT_BINARY,
+					attachmentInput.getFilename());
 		}
-		return postAttachments(attachmentsUri, entity);
+		return postAttachments(attachmentsUri, entityBuilder);
 	}
 
 	@Override
 	public Promise<Void> addAttachments(final URI attachmentsUri, final File... files) {
-		final MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, null, Charset.defaultCharset());
+		final MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create()
+				.setLaxMode()
+				.setCharset(UTF_8);
 		for (final File file : files) {
-			entity.addPart(FILE_BODY_TYPE, new FileBody(file));
+			entityBuilder.addBinaryBody(FILE_BODY_TYPE, file);
 		}
-		return postAttachments(attachmentsUri, entity);
+		return postAttachments(attachmentsUri, entityBuilder);
 	}
 
 	@Override
@@ -335,14 +377,7 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 
 	@Override
 	public Promise<InputStream> getAttachment(URI attachmentUri) {
-		return callAndParse(client().newRequest(attachmentUri).get(),
-				new ResponseHandler<InputStream>() {
-					@Override
-					public InputStream handle(final Response request) throws JSONException, IOException {
-						return request.getEntityStream();
-					}
-				}
-		);
+		return callAndParse(client().newRequest(attachmentUri).get(), Message::getEntityStream);
 	}
 
 	@Override
@@ -362,10 +397,10 @@ public class AsynchronousIssueRestClient extends AbstractAsynchronousRestClient 
 		return post(uriBuilder.build(), worklogInput, new WorklogInputJsonGenerator());
 	}
 
-	private Promise<Void> postAttachments(final URI attachmentsUri, final MultipartEntity multipartEntity) {
+	private Promise<Void> postAttachments(final URI attachmentsUri, final MultipartEntityBuilder multipartEntityBuilder) {
 		final ResponsePromise responsePromise = client()
 				.newRequest(attachmentsUri)
-				.setEntity(new MultiPartEntityBuilder(multipartEntity))
+				.setEntity(new MultiPartEntityBuilder(multipartEntityBuilder.build()))
 				.setHeader("X-Atlassian-Token", "nocheck")
 				.post();
 		return call(responsePromise);
