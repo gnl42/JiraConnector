df304bc10182e641f11bd268534a37cf12adc545
George Lindholm <George.Lindholm@ubc.ca>
2022-05-01 11:23:36 -0700

    Revert "Some prep work for adding watchers"
    This reverts commit 042736cc99a105d82b2984c1aaf645008f46ff0d.

--- jira.core/src/me/glindholm/connector/eclipse/internal/jira/core/IJiraConstants.java
+++ jira.core/src/me/glindholm/connector/eclipse/internal/jira/core/IJiraConstants.java
@@ -13,72 +13,70 @@ package me.glindholm.connector.eclipse.internal.jira.core;
 
 public class IJiraConstants {
 
-    public static final String ATTRIBUTE_TYPE = "attribute.jira.type"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_TYPE = "attribute.jira.type"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ISSUE_PARENT_KEY = "attribute.jira.issue_parent_key"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ISSUE_PARENT_KEY = "attribute.jira.issue_parent_key"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ISSUE_PARENT_ID = "attribute.jira.issue_parent_id"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ISSUE_PARENT_ID = "attribute.jira.issue_parent_id"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ENVIRONMENT = "attribute.jira.environment"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ENVIRONMENT = "attribute.jira.environment"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_COMPONENTS = "attribute.jira.components"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_COMPONENTS = "attribute.jira.components"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_FIXVERSIONS = "attribute.jira.fixversions"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_FIXVERSIONS = "attribute.jira.fixversions"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_AFFECTSVERSIONS = "attribute.jira.affectsversions"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_AFFECTSVERSIONS = "attribute.jira.affectsversions"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ESTIMATE = "attribute.jira.estimate"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ESTIMATE = "attribute.jira.estimate"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_INITIAL_ESTIMATE = "attribute.jira.initialestimate"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_INITIAL_ESTIMATE = "attribute.jira.initialestimate"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ACTUAL = "attribute.jira.actual"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ACTUAL = "attribute.jira.actual"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_DUE_DATE = "attribute.jira.due"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_DUE_DATE = "attribute.jira.due"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_SUBTASK_IDS = "attribute.jira.subtask_ids"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_SUBTASK_IDS = "attribute.jira.subtask_ids"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_SUBTASK_KEYS = "attribute.jira.subtask_keys"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_SUBTASK_KEYS = "attribute.jira.subtask_keys"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_CUSTOM_PREFIX = "attribute.jira.custom::"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_CUSTOM_PREFIX = "attribute.jira.custom::"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_LINK_PREFIX = "attribute.jira.link::"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_LINK_PREFIX = "attribute.jira.link::"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_SECURITY_LEVEL = "attribute.jira.security"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_SECURITY_LEVEL = "attribute.jira.security"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_READ_ONLY = "attribute.jira.read-only"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_READ_ONLY = "attribute.jira.read-only"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_WORKLOG_NOT_SUPPORTED = "attribute.jira.worklog-not-supported"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_WORKLOG_NOT_SUPPORTED = "attribute.jira.worklog-not-supported"; //$NON-NLS-1$
 
-    public static final String JIRA_DATE_FORMAT = "dd MMM yyyy HH:mm:ss z"; //$NON-NLS-1$
+	public static final String JIRA_DATE_FORMAT = "dd MMM yyyy HH:mm:ss z"; //$NON-NLS-1$
 
-    public static final String META_TYPE = "type"; //$NON-NLS-1$
+	public static final String META_TYPE = "type"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_LINKED_IDS = "attribute.jira.link_ids"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_LINKED_IDS = "attribute.jira.link_ids"; //$NON-NLS-1$
 
-    public static final String META_SUB_TASK_TYPE = "isSubtaskType"; //$NON-NLS-1$
+	public static final String META_SUB_TASK_TYPE = "isSubtaskType"; //$NON-NLS-1$
 
-    public static final String JIRA_TOOLKIT_PREFIX = "com.atlassian.jira.toolkit"; //$NON-NLS-1$
+	public static final String JIRA_TOOLKIT_PREFIX = "com.atlassian.jira.toolkit"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_PROJECT_ROLES = "attribute.jira.project-roles"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_PROJECT_ROLES = "attribute.jira.project-roles"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_VOTES = "attribute.jira.votes"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_VOTES = "attribute.jira.votes"; //$NON-NLS-1$
 
-    public static final String NEW_COMMENT_VIEWABLE_BY_ALL = "All Users"; //$NON-NLS-1$
+	public static final String NEW_COMMENT_VIEWABLE_BY_ALL = "All Users"; //$NON-NLS-1$
 
-    public static final String COMMENT_SECURITY_LEVEL = "task.common.comment.level"; //$NON-NLS-1$
+	public static final String COMMENT_SECURITY_LEVEL = "task.common.comment.level"; //$NON-NLS-1$
 
-    public static final String TYPE_NUMBER = "type_number"; //$NON-NLS-1$
+	public static final String TYPE_NUMBER = "type_number"; //$NON-NLS-1$
 
-    public static final String PREFERENCE_SECURE_STORAGE_MIGRATED = "secure_storage.migration.done"; //$NON-NLS-1$
+	public static final String PREFERENCE_SECURE_STORAGE_MIGRATED = "secure_storage.migration.done"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_JIRA_LOGGED_ACTIVITY_TIME = "attribute.jira.loggedActivityTime"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_JIRA_LOGGED_ACTIVITY_TIME = "attribute.jira.loggedActivityTime"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_ADJUST_ESTIMATE_OPTION = "attribute.adjust-estimate"; //$NON-NLS-1$
+	public static final String ATTRIBUTE_ADJUST_ESTIMATE_OPTION = "attribute.adjust-estimate"; //$NON-NLS-1$
 
-    public static final String WORKLOG_NEW = "attribute.jira.worklog.new"; //$NON-NLS-1$
+	public static final String WORKLOG_NEW = "attribute.jira.worklog.new"; //$NON-NLS-1$
 
-    public static final String ATTRIBUTE_LABELS = "attribute.jira.labels"; //$NON-NLS-1$
-
-    public static final String ATTRIBUTE_WATCHERS = "attribute.jira.watchers";
+	public static final String ATTRIBUTE_LABELS = "attribute.jira.labels"; //$NON-NLS-1$
 
 }
--- jira.core/src/me/glindholm/connector/eclipse/internal/jira/core/Messages.java
+++ jira.core/src/me/glindholm/connector/eclipse/internal/jira/core/Messages.java
@@ -14,124 +14,122 @@ package me.glindholm.connector.eclipse.internal.jira.core;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-    private static final String BUNDLE_NAME = "me.glindholm.connector.eclipse.internal.jira.core.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "me.glindholm.connector.eclipse.internal.jira.core.messages"; //$NON-NLS-1$
 
-    static {
-        // load message values from bundle file
-        reloadMessages();
-    }
+	static {
+		// load message values from bundle file
+		reloadMessages();
+	}
 
-    public static void reloadMessages() {
-        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-    }
+	public static void reloadMessages() {
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
 
-    public static String JiraAttribute_Affects_Versions;
+	public static String JiraAttribute_Affects_Versions;
 
-    public static String JiraAttribute_Assigned_to;
+	public static String JiraAttribute_Assigned_to;
 
-    public static String JiraAttribute_Components;
+	public static String JiraAttribute_Components;
 
-    public static String JiraAttribute_Created;
+	public static String JiraAttribute_Created;
 
-    public static String JiraAttribute_Description;
+	public static String JiraAttribute_Description;
 
-    public static String JiraAttribute_DUEDATE;
+	public static String JiraAttribute_DUEDATE;
 
-    public static String JiraAttribute_Environment;
+	public static String JiraAttribute_Environment;
 
-    public static String JiraAttribute_Estimate;
+	public static String JiraAttribute_Estimate;
 
-    public static String JiraAttribute_Fix_Versions;
+	public static String JiraAttribute_Fix_Versions;
 
-    public static String JiraAttribute_Key;
+	public static String JiraAttribute_Key;
 
-    public static String JiraAttribute_Linked_ids;
+	public static String JiraAttribute_Linked_ids;
 
-    public static String JiraAttribute_Modified;
+	public static String JiraAttribute_Modified;
 
-    public static String JiraAttribute_New_Comment;
+	public static String JiraAttribute_New_Comment;
 
-    public static String JiraAttribute_Viewable_by;
+	public static String JiraAttribute_Viewable_by;
 
-    public static String JiraAttribute_Original_Estimate;
+	public static String JiraAttribute_Original_Estimate;
 
-    public static String JiraAttribute_Parent;
+	public static String JiraAttribute_Parent;
 
-    public static String JiraAttribute_Parent_ID;
+	public static String JiraAttribute_Parent_ID;
 
-    public static String JiraAttribute_Priority;
+	public static String JiraAttribute_Priority;
 
-    public static String JiraAttribute_Project;
+	public static String JiraAttribute_Project;
 
-    public static String JiraAttribute_Reported_by;
+	public static String JiraAttribute_Reported_by;
 
-    public static String JiraAttribute_Resolution;
+	public static String JiraAttribute_Resolution;
 
-    public static String JiraAttribute_Security_Level;
+	public static String JiraAttribute_Security_Level;
 
-    public static String JiraAttribute_Status;
+	public static String JiraAttribute_Status;
 
-    public static String JiraAttribute_Subtask_ids;
+	public static String JiraAttribute_Subtask_ids;
 
-    public static String JiraAttribute_Subtasks;
+	public static String JiraAttribute_Subtasks;
 
-    public static String JiraAttribute_Summary;
+	public static String JiraAttribute_Summary;
 
-    public static String JiraAttribute_Time_Spent;
+	public static String JiraAttribute_Time_Spent;
 
-    public static String JiraAttribute_Type;
+	public static String JiraAttribute_Type;
 
-    public static String JiraAttribute_unknown;
+	public static String JiraAttribute_unknown;
 
-    public static String JiraAttribute_URL;
+	public static String JiraAttribute_URL;
 
-    public static String JiraAttribute_Votes;
+	public static String JiraAttribute_Votes;
 
-    public static String JiraAttribute_Labels;
+	public static String JiraAttribute_Labels;
 
-    public static String JiraAttribute_Watchers;
+	public static String JiraCorePlugin_JIRA_description;
 
-    public static String JiraCorePlugin_JIRA_description;
+	public static String JiraRepositoryConnector_Getting_changed_tasks;
 
-    public static String JiraRepositoryConnector_Getting_changed_tasks;
+	public static String JiraRepositoryConnector_The_JIRA_query_is_invalid;
 
-    public static String JiraRepositoryConnector_The_JIRA_query_is_invalid;
+	public static String JiraRepositoryConnector_Query_Repository;
 
-    public static String JiraRepositoryConnector_Query_Repository;
+	public static String JiraRepositoryConnector_The_repository_returned_an_unknown_project;
 
-    public static String JiraRepositoryConnector_The_repository_returned_an_unknown_project;
+	public static String JiraTaskAttachmentHandler_Getting_attachment;
 
-    public static String JiraTaskAttachmentHandler_Getting_attachment;
+	public static String JiraTaskAttachmentHandler_Sending_attachment;
 
-    public static String JiraTaskAttachmentHandler_Sending_attachment;
+	public static String JiraTaskDataHandler_Creating_subtask;
 
-    public static String JiraTaskDataHandler_Creating_subtask;
+	public static String JiraTaskDataHandler_Getting_task;
 
-    public static String JiraTaskDataHandler_Getting_task;
+	public static String JiraTaskDataHandler_Leave;
 
-    public static String JiraTaskDataHandler_Leave;
+	public static String JiraTaskDataHandler_Leave_as_X;
 
-    public static String JiraTaskDataHandler_Leave_as_X;
+	public static String JiraTaskDataHandler_Sending_task;
 
-    public static String JiraTaskDataHandler_Sending_task;
+	public static String WorkLogConverter_Auto_Adjust_Estimate;
 
-    public static String WorkLogConverter_Auto_Adjust_Estimate;
+	public static String WorkLogConverter_Author;
 
-    public static String WorkLogConverter_Author;
+	public static String WorkLogConverter_Comment;
 
-    public static String WorkLogConverter_Comment;
+	public static String WorkLogConverter_Created;
 
-    public static String WorkLogConverter_Created;
+	public static String WorkLogConverter_Group_Level;
 
-    public static String WorkLogConverter_Group_Level;
+	public static String WorkLogConverter_Id;
 
-    public static String WorkLogConverter_Id;
+	public static String WorkLogConverter_Role_Level;
 
-    public static String WorkLogConverter_Role_Level;
+	public static String WorkLogConverter_Start_Date;
 
-    public static String WorkLogConverter_Start_Date;
+	public static String WorkLogConverter_Time;
 
-    public static String WorkLogConverter_Time;
-
-    public static String WorkLogConverter_Updated;
+	public static String WorkLogConverter_Updated;
 }
